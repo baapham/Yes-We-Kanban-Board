c0b074feda34880d460909fc152c14cf5e5dd681
Alex Pham <z5209652@ad.unsw.edu.au>
2020-02-19 22:33:12 +1100

    added firebase

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeProject } from '../actions/projects';
 
 const ProjectItem = props => {
   return (
     <div>
       <h3>{props.project.title}</h3>
       <p>{props.project.description}</p>
+      <button
+        onClick={() => {
+          props.dispatch(removeProject(props.project));
+        }}
+      >
+        Remove
+        {process.env.FIREBASE_API_KEY}
+      </button>
     </div>
   );
 };
 
-export default ProjectItem;
+export default connect()(ProjectItem);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,15 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import projectsReducer from '../reducers/projects';
+// import thunk from 'redux-thunk';
 
+// const composeEnhancers =
+// window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
       projects: projectsReducer,
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // composeEnhancers(applyMiddleware(thunk)),
   );
   return store;
 };
