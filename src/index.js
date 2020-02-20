9ce14f5bd9dec25e1dda70aab5a298064ce7386b
Alex Pham <z5209652@ad.unsw.edu.au>
2020-02-20 23:26:33 +1100

    trying to connect to firebase

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import AppRouter from './routers/AppRouter';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store/configureStore';
 import { addProject, removeProject } from './actions/projects';
+import database from './firebase/firebase';
 
 const store = configureStore();
 
@@ -24,6 +25,18 @@ store.dispatch(
   }),
 );
 console.log(store.getState());
+
+database.ref('expenses').on('child_added', snapshot => {
+  console.log(snapshot.key, snapshot.val());
+});
+let expense = {
+  description: 'Rent',
+  note: 'This is rent pay',
+  amount: 200,
+  createdAt: 0,
+};
+
+database.ref('expenses').push(expense);
 
 const jsx = (
   <Provider store={store}>
